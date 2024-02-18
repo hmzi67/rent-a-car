88d2a1d7092487ecfbe1376edafae3b88394da66
Hamza Waheed <137566252+hmzi67@users.noreply.github.com>
2024-02-19 02:16:53 +0500

    create signup API routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 const mongoDB = process.env.MONGODB_URL;
@@ -15,9 +16,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json())
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!! no light");
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter)
